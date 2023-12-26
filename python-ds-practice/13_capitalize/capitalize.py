6651dd7c32443fbdd1eae528245e4c30f52ebe40
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 15:22:58 -0800

    Completed Exercise 13: Capitalize

--- python-ds-practice/13_capitalize/capitalize.py
+++ python-ds-practice/13_capitalize/capitalize.py
@@ -7,3 +7,8 @@ def capitalize(phrase):
         >>> capitalize('only first word')
         'Only first word'
     """
+    first_letter_index = 0
+    while not phrase[first_letter_index].isalpha():
+        first_letter_index += 1
+    first_letter_capitalized = phrase[first_letter_index].upper()
+    return phrase[0:first_letter_index] + first_letter_capitalized + phrase[first_letter_index+1:]
