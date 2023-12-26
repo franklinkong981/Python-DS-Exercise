c17f222a867f585955bcf63441cc2f94de3a6978
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 14:43:45 -0800

    Completed Exercise 7: Multiple Letter Counts

--- python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
+++ python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,10 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letter_counts = {}
+    for char in phrase:
+        if char not in letter_counts:
+            letter_counts[char] = 1
+        else:
+            letter_counts[char] += 1
+    return letter_counts
