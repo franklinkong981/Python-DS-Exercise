38ece638923bed859cb52dd5855bce29486dacc3
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 19:24:36 -0800

    Completed Exercise 24: Remove Every Other

--- python-ds-practice/24_remove_every_other/remove_every_other.py
+++ python-ds-practice/24_remove_every_other/remove_every_other.py
@@ -11,3 +11,10 @@ def remove_every_other(lst):
         >>> lst
         [1, 2, 3, 4, 5]
     """
+    every_other_list = []
+    current_index = 0
+    for number in lst:
+        if current_index % 2 == 0:
+            every_other_list.append(number)
+        current_index += 1
+    return every_other_list
