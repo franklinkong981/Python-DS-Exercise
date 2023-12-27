ba85c4783b2df6ced118dc2b354010fdcc4ec38c
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 17:27:31 -0800

    Completed Exercise 19: Friend Date

--- python-ds-practice/19_friend_date/friend_date.py
+++ python-ds-practice/19_friend_date/friend_date.py
@@ -16,3 +16,8 @@ def friend_date(a, b):
         >>> friend_date(sauron, gandalf)
         True
     """
+    hobby_list1 = set(a[2])
+    hobby_list2 = set(b[2])
+    if len(list(hobby_list1 & hobby_list2)) > 0:
+        return True
+    return False
