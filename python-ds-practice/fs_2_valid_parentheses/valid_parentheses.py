cf2bf450869ad5edffa9a5c0d042f761e8cd469f
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 22:29:45 -0800

    Completed Bonus Exercise 2: Valid Parentheses

--- python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
+++ python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,12 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    balanced = 0
+    for char in parens:
+        if char == '(':
+            balanced += 1
+        elif char == ')':
+            balanced -= 1
+        if balanced < 0:
+            return False
+    return balanced == 0
