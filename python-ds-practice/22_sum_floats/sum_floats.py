4e640322c3179479b7f8b14e7654a7c9f37fcffb
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 19:19:15 -0800

    Completed Exercise 22: Sum Floats

--- python-ds-practice/22_sum_floats/sum_floats.py
+++ python-ds-practice/22_sum_floats/sum_floats.py
@@ -11,3 +11,8 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    sum_of_floats = 0
+    for item in nums:
+        if isinstance(item, float):
+            sum_of_floats += item
+    return sum_of_floats
