b917abd832adb9d242d76d524bf6443d468e9740
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 21:35:00 -0800

    Completed Exercise 34: Same Frequency

--- python-ds-practice/34_same_frequency/same_frequency.py
+++ python-ds-practice/34_same_frequency/same_frequency.py
@@ -10,3 +10,17 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    numbers_list = []
+    while num1 != 0:
+        numbers_list.append(num1 % 10)
+        num1 = int(num1 / 10)
+    while num2 != 0:
+        number_to_remove = num2 % 10
+        if number_to_remove not in numbers_list:
+            return False
+        index_to_remove = numbers_list.index(number_to_remove)
+        numbers_list.pop(index_to_remove)
+        num2 = int(num2 / 10)
+    if len(numbers_list) != 0:
+        return False
+    return True 
