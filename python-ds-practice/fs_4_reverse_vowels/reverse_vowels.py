baa4a3936e579c5037e415b99d7bbc78a2c5ed16
Franklin Kong <franklinkong981@gmail.com>
2023-12-26 22:47:44 -0800

    Completed Bonus Exercise 4: Reverse Vowels

--- python-ds-practice/fs_4_reverse_vowels/reverse_vowels.py
+++ python-ds-practice/fs_4_reverse_vowels/reverse_vowels.py
@@ -19,3 +19,16 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    left_index = 0
+    right_index = len(s) - 1
+    reverse_vowels_string = s
+    while left_index < right_index:
+        if s[left_index] not in 'AEIOUaeiou':
+            left_index += 1
+        if s[right_index] not in 'AEIOUaeiou':
+            right_index -= 1
+        if left_index < right_index and s[left_index] in 'AEIOUaeiou' and s[right_index] in 'AEIOUaeiou':
+            reverse_vowels_string = reverse_vowels_string[0:left_index] + s[right_index] + reverse_vowels_string[left_index+1:right_index] + s[left_index] + reverse_vowels_string[right_index+1:]
+            left_index += 1
+            right_index -= 1
+    return reverse_vowels_string 
